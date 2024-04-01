65b02a7f013ad34c83d5beee44383278b5fa8b36
Jawadmurad <133027717+Jawadmurad@users.noreply.github.com>
2024-04-02 03:47:27 +0400

    Add files via upload

---
+++ StripeProject/StripeProject/Controllers/StripeController.cs
@@ -0,0 +1,69 @@
+﻿namespace StripeProject.Controllers;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using StripeProject.Dtos;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StripeController : ControllerBase
+{
+
+    [HttpPost]
+    public async Task<IActionResult> AddInvoiceAsync([FromBody] AddInvoiceDto addInvoiceDto)
+    {
+        var customerId = await CustomerHanleAsync(addInvoiceDto);
+        var invoice = await CreateInvoiceAsync(addInvoiceDto, customerId);
+
+        var paymentService = new PaymentIntentService();
+        var payment = await paymentService.GetAsync(invoice.PaymentIntentId);
+
+        return Ok(payment.PaymentMethodOptions.Link);
+
+    }
+
+    private static async Task<Invoice> CreateInvoiceAsync(AddInvoiceDto addInvoiceDto, string customerId)
+    {
+        var invoiceOption = new InvoiceCreateOptions
+        {
+            Customer = customerId,
+            Description = $"invocie number {addInvoiceDto.InvoiceNumber}"
+        };
+
+        var invoiceService = new InvoiceService();
+        var invoice = await invoiceService.CreateAsync(invoiceOption);
+
+        invoice.CustomerId = customerId;
+        invoice.AccountCountry = addInvoiceDto.CurrencyCode;
+        invoice.AmountPaid = addInvoiceDto.InvoiceAmount;
+        invoice.AccountName = addInvoiceDto.ServiceName;
+
+        return invoice;
+    }
+
+    private static async Task<string> CustomerHanleAsync(AddInvoiceDto addInvoiceDto)
+    {
+        var searchCustomer = new CustomerSearchOptions
+        {
+            Query = $"email : \"{addInvoiceDto.CustomerEmail}\"",
+        };
+
+        var customerService = new CustomerService();
+        var customer = await customerService.SearchAsync(searchCustomer);
+
+        if (customer is null)
+        {
+            var createCustomer = new CustomerCreateOptions
+            {
+                Name = $"{addInvoiceDto.CustomerName}",
+                Email = $"{addInvoiceDto.CustomerEmail}",
+                Phone = $"{addInvoiceDto.CustomerPhone}"
+            };
+
+            await customerService.CreateAsync(createCustomer);
+        }
+
+        return customer!.First().Id;
+    }
+}
---
+++ StripeProject/StripeProject/Dtos/AddInvoiceDto.cs
@@ -0,0 +1,18 @@
+﻿namespace StripeProject.Dtos;
+
+public class AddInvoiceDto
+{
+    public string InvoiceNumber { get; set; } = default!;
+
+    public string CustomerName { get; set; } = default!;
+
+    public string CustomerEmail { get; set; } = default!;
+
+    public string CustomerPhone { get; set; } = default!;
+
+    public long InvoiceAmount { get; set; } = default!;
+
+    public string CurrencyCode { get; set; } = default!;
+
+    public string ServiceName { get; set; } = default!;
+}
